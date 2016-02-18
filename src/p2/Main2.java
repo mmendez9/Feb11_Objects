054c473527ee267a748eba29b00ef7e14365bed3
mmendez9 <hikari1995>
2016-02-18 08:58:14 -0500

    Integer object

---
+++ src/p2/Main2.java
@@ -0,0 +1,14 @@
+package p2;
+
+/*
+ * Created by mmendez9 on 2/16/2016.
+ */
+public class Main2 {
+    public static void main(String[] joe) {
+        int i =25;
+
+        Integer iObject = new Integer(i);
+
+        System.out.println("Result = " + Integer.parseInt("28")/ 2);
+    }
+}
